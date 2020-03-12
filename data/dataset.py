2686f8b2bfba01ce2cf87a6077937e1712cf0e4c
Sergey Malk <errorinever@gmail.com>
2020-03-12 03:24:55 +0300

    fix bug

--- data/dataset.py
+++ data/dataset.py
@@ -18,10 +18,10 @@ class XRayDataset(Dataset):
 
     def __getitem__(self, idx):
         img_path = self.df.iloc[idx, 0]
-        label = 0 if self.df.iloc[idx, 1] == 'NORMAL' else 1
+        label = torch.tensor([0]) if self.df.iloc[idx, 1] == 'NORMAL' else torch.tensor([1])
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = kornia.image_to_tensor(img)
-        return dict(images=img, label=label)
+        return img, label
 
     def __len__(self):
         return len(self.df)
