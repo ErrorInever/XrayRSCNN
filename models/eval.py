a7ac65f88576e97a628ae4b59a41d3505745f6b9
Sergey Malk <errorinever@gmail.com>
2020-04-03 22:35:11 +0300

    fix error dim tensor

--- models/eval.py
+++ models/eval.py
@@ -18,10 +18,14 @@ def evaluate(model, dataloader, loss, device):
 
         predicts = model.forward(images)
 
-        loss_value = loss(predicts, labels.squeeze())
-
-        running_loss += loss_value.item()
-        running_acc += (predicts.argmax() == labels.data).float().mean()
+        # if labels.dim() == 1 after squeeze() tensor will not have dimension
+        try:
+            loss_value = loss(predicts, labels.squeeze())
+        except IndexError:
+            loss_value = loss(predicts, labels.squeeze(0))
+        finally:
+            running_loss += loss_value.item()
+            running_acc += (predicts.argmax() == labels.data).float().mean()
 
     return running_loss, running_acc
 
