db6ef1c52a1c9a9c1fe5bef19215a044ff634781
Sergey Malk <errorinever@gmail.com>
2020-04-24 22:00:01 +0300

    add eval"

---
+++ models/eval.py
@@ -0,0 +1,28 @@
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def evaluate(model, dataloader, criterion, device, epoch, print_freq=100):
+    logger.setLevel(logging.INFO)
+
+    model.eval()
+
+    running_loss = 0.
+    running_acc = 0.
+
+    for i, (image, label) in enumerate(dataloader):
+        images = image.to(device)
+        labels = label.to(device)
+
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+
+        running_loss += loss.item()
+        running_acc += (outputs.argmax(dim=1) == labels).float().mean().item()
+
+        if i % print_freq == 0:
+            logger.info('Evaluate [%s, %s] Loss: %s | Acc: %s', epoch + 1, i + 1,
+                        running_loss / print_freq, running_acc / print_freq)
+            running_loss = 0.
+            running_acc = 0.
