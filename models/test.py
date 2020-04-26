bf16fa0cb84d03797991ff4f92a93f2d066312c9
Sergey Malk <errorinever@gmail.com>
2020-04-26 23:29:49 +0300

    add tests

---
+++ models/test.py
@@ -0,0 +1,21 @@
+import logging
+from tqdm import tqdm
+
+logger = logging.getLogger(__name__)
+
+
+def test(model, dataloader, device):
+    logger.setLevel(logging.INFO)
+    model.eval()
+    test_acc = 0.
+    for images, labels in tqdm(dataloader, total=len(dataloader)):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+        predict_classes = outputs.argmax(dim=1)
+        test_acc += (predict_classes == labels.data).float().mean()
+
+    test_acc = test_acc / len(dataloader)
+
+    logger.info('Test accuracy: %s', test_acc)
