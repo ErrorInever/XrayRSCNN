ea9dfe77c78550cb1bd347718ef2e79e06c9217d
Sergey Malk <errorinever@gmail.com>
2020-05-18 19:10:16 +0300

    refactoring

--- data/dataset.py
+++ data/dataset.py
@@ -12,7 +12,7 @@ class Images(Dataset):
 
     def __init__(self, img_path):
         self.img_path = img_path
-        self.img_names = [n for n in os.listdir(img_path)]
+        self.img_names = [n for n in os.listdir(img_path) if n.endswith(('png', 'jpeg', 'jpg'))]
 
     def __getitem__(self, idx):
         img = Image.open(os.path.join(self.img_path, self.img_names[idx])).convert('RGB')
@@ -24,7 +24,7 @@ class Images(Dataset):
 
     @property
     def transform(self):
-        return transforms.Compose([transforms.ToTensor()])
+        return transforms.Compose([transforms.Resize(224, 224), transforms.ToTensor()])
 
 
 class XrayImageFolder(ImageFolder):
@@ -38,50 +38,50 @@ class XrayImageFolder(ImageFolder):
         return sample, target
 
 
-class XRayDataset(Dataset):
-    """X-Ray dataset"""
-
-    def __init__(self, df, transforms=None):
-        """
-        :param df: ``DataFrame`` with columns [img_path, label]
-        :param transforms: if True then allow to transform
-        """
-        self.df = df
-        self.transforms = transforms
-
-    def __getitem__(self, idx):
-        img_path = self.df.iloc[idx, 0]
-        label = torch.tensor([0]) if self.df.iloc[idx, 1] == 'NORMAL' else torch.tensor([1])
-        img = cv2.imread(img_path)
-
-        if self.transforms:
-            img = self.transform(img)
-        else:
-            val_transform = transforms.Compose([
-                transforms.ToTensor()
-            ])
-            img = val_transform(img)
-
-        return img, label
-
-    def __len__(self):
-        return len(self.df)
-
-    @property
-    def transform(self):
-        return transforms.Compose([
-            iaa.Sequential([
-                iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=(0, 4.0))),
-                iaa.Fliplr(0.2),
-                iaa.Affine(rotate=(-20, 20), mode='symmetric'),
-                iaa.Multiply(0.50),
-                iaa.Sometimes(0.25, iaa.OneOf([iaa.Dropout(p=(0, 0.1)),
-                                               iaa.CoarseDropout(0.1, size_percent=0.5)
-                                               ])),
-                iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True),
-                iaa.Sometimes(0.10, iaa.SaltAndPepper(0.03), iaa.LogContrast(gain=0.5))
-            ]).augment_image,
-            transforms.ToTensor(),
-            #transforms.Normalize(mean=[0.1581, 0.1562, 0.1562], std=[0.0756, 0.0751, 0.0751])
-            #transforms.Normalize(mean=[0.4819, 0.4819, 0.4819], std=[0.2396, 0.2396, 0.2396])
-        ])
+# class XRayDataset(Dataset):
+#     """X-Ray dataset"""
+#
+#     def __init__(self, df, transforms=None):
+#         """
+#         :param df: ``DataFrame`` with columns [img_path, label]
+#         :param transforms: if True then allow to transform
+#         """
+#         self.df = df
+#         self.transforms = transforms
+#
+#     def __getitem__(self, idx):
+#         img_path = self.df.iloc[idx, 0]
+#         label = torch.tensor([0]) if self.df.iloc[idx, 1] == 'NORMAL' else torch.tensor([1])
+#         img = cv2.imread(img_path)
+#
+#         if self.transforms:
+#             img = self.transform(img)
+#         else:
+#             val_transform = transforms.Compose([
+#                 transforms.ToTensor()
+#             ])
+#             img = val_transform(img)
+#
+#         return img, label
+#
+#     def __len__(self):
+#         return len(self.df)
+#
+#     @property
+#     def transform(self):
+#         return transforms.Compose([
+#             iaa.Sequential([
+#                 iaa.Sometimes(0.25, iaa.GaussianBlur(sigma=(0, 4.0))),
+#                 iaa.Fliplr(0.2),
+#                 iaa.Affine(rotate=(-20, 20), mode='symmetric'),
+#                 iaa.Multiply(0.50),
+#                 iaa.Sometimes(0.25, iaa.OneOf([iaa.Dropout(p=(0, 0.1)),
+#                                                iaa.CoarseDropout(0.1, size_percent=0.5)
+#                                                ])),
+#                 iaa.AddToHueAndSaturation(value=(-10, 10), per_channel=True),
+#                 iaa.Sometimes(0.10, iaa.SaltAndPepper(0.03), iaa.LogContrast(gain=0.5))
+#             ]).augment_image,
+#             transforms.ToTensor(),
+#             #transforms.Normalize(mean=[0.1581, 0.1562, 0.1562], std=[0.0756, 0.0751, 0.0751])
+#             #transforms.Normalize(mean=[0.4819, 0.4819, 0.4819], std=[0.2396, 0.2396, 0.2396])
+#         ])
